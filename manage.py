4aa4fd4db0465b518fb22d634ca7c84e33f07e4e
Yuri Istomin <yura.ist@icloud.com>
2018-02-11 15:27:35 +0300

    Envars importing from .env file

--- manage.py
+++ manage.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 import os
+
+
+if os.path.exists('.env'):
+    print('Importing environment from .env ...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) > 2:
+            os.environ[var[0]] = var[1]
+
+
 from app import create_app, db
 from app.models import User, Role
 from flask_script import Manager, Shell
